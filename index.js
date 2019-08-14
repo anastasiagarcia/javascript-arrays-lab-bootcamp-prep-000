ad2c606edadd365d9f453eaeb341ce4211eb8e55
Anastasia Pankina <anastaciap@ymail.com>
2019-08-14 14:55:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,8 +39,9 @@ function prependKitten(name){
   return kitten;
 }
 
-//removing indexes fromarray without changing the existing array
-//
+//removing indexes from array without changing the existing array
+//The slice() method returns the selected elements in an array, as a new array object 
+//for example kittens.slice(1,2); it starts from index 1 (not include until index 2 include);
 
 function removeLastKitten(){
   var kitten= kittens.slice(0, kittens.length-1);
