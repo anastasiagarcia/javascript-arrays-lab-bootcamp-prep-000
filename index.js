7c6fb2ad08dd5f045f7b6741a4b105fc7d610553
Anastasia Pankina <anastaciap@ymail.com>
2019-08-14 14:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,3 +28,7 @@ function destructivelyRemoveFirstKitten() {
 
 //append a kitten to the kittens array and return a new kittens array leaving the kitten array unchanged
 
+function appendKitten(name){
+  var kittens=(...kittens);
+  return kittens;
+}
